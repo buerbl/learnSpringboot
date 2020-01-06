b2e7b2d9ccbd39a960761a25994019ef18d28f55
buer <18319748706@163.com>
2020-01-06 16:41:11 +0800

    redis集成

---
+++ lsb-mq/src/main/java/com/example/lsbmq/LsbMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.lsbmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsbMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsbMqApplication.class, args);
+    }
+
+}

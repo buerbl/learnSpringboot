f9b3b02090113c43ff440fc734c96d80a6fe9fca
buer <18319748706@163.com>
2020-01-22 19:16:23 +0800

    :star:swageer

--- lsb-swagger/src/main/java/com/example/lsbswagger/controller/controller.java
+++ lsb-swagger/src/main/java/com/example/lsbswagger/controller/controller.java
@@ -3,7 +3,6 @@ package com.example.lsbswagger.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**

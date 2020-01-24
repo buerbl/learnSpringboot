d9e8c589cc537e7b0c67109d586fc59cdc1df3a4
buer <18319748706@163.com>
2020-01-24 12:52:59 +0800

    :memo:security

---
+++ lsb-security/README.md
@@ -0,0 +1,8 @@
+## 登录
+
+![](http://javahouse.xyz/20200124125212.png)
+
+## 注销
+
+![](http://javahouse.xyz/20200124125153.png)
+
--- lsb-security/src/main/java/com/example/lsbsecurity/config/MvcConfig.java
+++ lsb-security/src/main/java/com/example/lsbsecurity/config/MvcConfig.java
@@ -14,6 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/logout").setViewName("logout");
     }
 
 }

87a487bccb5e45c5304764d5b3ad38ccc9498959
buerbl <18319748706@163.com>
2022-10-11 23:44:18 +0800

    分页查询

--- lsb-crub-plus/src/test/java/com/example/lsbcrubplus/LsbCrubPlusApplicationTests.java
+++ lsb-crub-plus/src/test/java/com/example/lsbcrubplus/LsbCrubPlusApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.lsbcrubplus;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.lsbcrudplus.LsbCrubPlusApplication;
 import com.example.lsbcrudplus.entity.User;
 import com.example.lsbcrudplus.service.IUserService;
@@ -51,9 +53,22 @@ public class LsbCrubPlusApplicationTests {
     @Test
     public void testQuery(){
         LambdaQueryWrapper<User> lambdaQuery = Wrappers.lambdaQuery();
-        lambdaQuery.eq(User::getId, "1").eq(User::getAdress, "ss").groupBy(User::getAdress);
-        List<User> list = userService.list(lambdaQuery.select(User::getAdress, User::getStatus, User::getId));
+        lambdaQuery.select(User::getAdress, User::getStatus, User::getId)
+                .eq(User::getId, "1")
+                .eq(User::getAdress, "ss")
+                .groupBy(User::getAdress);
+        List<User> list = userService.list(lambdaQuery);
         log.info("list的结果{}", list);
     }
 
+    /**
+     * 分页查询
+     * SELECT id,name,password,adress,status FROM user LIMIT ?,?
+     */
+    @Test
+    public void testPage(){
+        IPage<User> page = new Page<>(4, 6);   // 第一个参数表示页树，第二个表示每页多少行
+        IPage<User> page1 = userService.page(page, Wrappers.lambdaQuery());
+        log.info("page的结果是{}", page1);
+    }
 }
